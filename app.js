05a7bc3ba2edc14b3316b6fde6f2e50086a13944
Shyam Rathod <shyamnit17@gmail.com>
2026-06-02 11:51:02 +0530

    initializing data with this folder

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
+const Listing = require("./models/listing")
 main().then(() => {
-    console.log("connection succesful");
+    console.log("connection succesful to db");
 }).catch(err => {
     console.log(err);
 });
@@ -13,9 +13,22 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.get("/", (req, res) => {
+    res.send("hi i am root");
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "my new villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Goa",
+        country: "India",
+    });
+    await sampleListing.save();
+    console.log("succesful testing");
+});
+
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
-app.get("/", (req, res) => {
-    res.send("hi i am root");
-})
