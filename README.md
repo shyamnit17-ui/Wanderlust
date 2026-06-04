356b78c2be8b4e6e48aa7fea2ee9c3639b2995bb
Shyam Rathod <shyamnit17@gmail.com>
2026-06-04 15:40:38 +0530

    all the files added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # full-stack-project
 my first full stack project
+created backend server and basic crud operations functionality to  delete create read update the lsitings
+*** next step is to build the navbar and add styling;
--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const Listing = require("./models/listing")
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsmate = require("ejs-mate");
+
+
+
+
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, 'public')));
 main().then(() => {
     console.log("connection succesful to db");
 }).catch(err => {
@@ -12,23 +21,74 @@ main().then(() => {
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("hi i am root");
 });
+app.engine("ejs", ejsmate);
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "my new villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Goa",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("succesful testing");
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "my new villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Goa",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("succesful testing");
+// });
+
+app.get("/listings", async (req, res) => {
+
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+
+});
+// new route
+app.get("/listings/new", ((req, res) => {
+    res.render("listings/new.ejs")
+}));
+//show route
+app.get("/listings/:id", async (req, res) => {
+
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
 });
 
+//create route
+app.post("/listings", async (req, res) => {
+    const newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    console.log(newlisting);
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+//update route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+
+});
+//delete route 
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+
+})
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
