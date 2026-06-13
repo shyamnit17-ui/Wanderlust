3f7cde631fc40dbc0fc6e1c9d147f81b53a3e5e6
Shyam Rathod <shyamnit17@gmail.com>
2026-06-13 16:48:31 +0530

    Remove

---
+++ init/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const data = require("./data.js");
+// const Listing = require("../models/listing.js");
+// const { init } = require("../models/listing");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const Listing = require("../models/listing.js")
+main().then(() => {
+    console.log("connection succesful to db");
+}).catch(err => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(data.data);
+    console.log("data was initialized");
+};
+initDB();
